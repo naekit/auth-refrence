018db3223a71d756cdfb46f65eb8bcfb182293ff
naekit <mykyta.onishchuck@code.berlin>
2023-01-26 16:45:49 -0500

    logout function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import RootLayout from "./pages/Root"
 import { action as manipulateEventAction } from "./components/EventForm"
 import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter"
 import AuthenticationPage, { authAction } from "./pages/Authentication"
+import { logoutAction } from "./pages/Logout"
 
 const router = createBrowserRouter([
 	{
@@ -59,6 +60,10 @@ const router = createBrowserRouter([
 				path: "auth",
 				element: <AuthenticationPage />,
 				action: authAction,
+			},
+			{
+				path: "logout",
+				action: logoutAction,
 			},
 			{
 				path: "newsletter",
